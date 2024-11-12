c4328ea958cbbbbaafbc3e909d8eed7ae1088816
jghagemann <hagejotage@hotmail.com>
2024-11-12 15:09:47 -0300

    initial commit

---
+++ src/test/java/com/hagemann/nttbank/NttbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hagemann.nttbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
